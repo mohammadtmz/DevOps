7185e10d5f56e1989db929abdac2517a2981a98f
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-06 14:39:15 -0400

    Create S3andEC2.tf

---
+++ 02_Infrastructure_as_Code/Terraform/AWS_Terraform/S3andEC2.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "testbucket"{
+  bucket = "terraformexample"
+  acl = "private"
+
+  tags = {
+    Name = "MyS3Bucket"
+  }
+}
+
+resource "aws_instance" "example"{
+    ami = "ami-0182f373e66f89c85"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "TerraformTest"
+    }
+}
